3dd7a1c9522692fb1f9bf7a96051539d641fe37d
Dmitry Astapov <dastapov@gmail.com>
2020-05-25 22:49:11 +0100

    past-as-budget

--- README.md
+++ README.md
@@ -5,3 +5,7 @@ Collection of small Haskell snippets that use hledger-lib
 * `combine-balance-reports.hs` : allows you to put two balance reports side by side.
   For example, you can see how day/week/month/quarter from this year looks vs the
   same period from the previous year
+
+* `past-as-budget.hs` : allows you to use one balance report as "budget numbers" for
+  another balance report. For example, you can quicky see how your current expenses
+  line up to past year's expenses (both in absolute numbers and percents).
---
+++ past-as-budget.hs
@@ -0,0 +1,19 @@
+{-| Construct two balance reports for two different time periods and use one of the as "budget" for
+    the other, thus comparing them
+-}  
+import Hledger.Cli
+import Data.Default
+
+main :: IO ()
+main = do
+  d <- getCurrentDay
+  Right j2019 <- readJournalFile definputopts  "2019.journal"
+  Right j2020 <- readJournalFile definputopts  "2020.journal"
+  let o2019 = def{query_="expenses",average_=True,row_total_=True,period_=MonthPeriod 2019 4}
+  let o2020 = def{query_="expenses",average_=True,row_total_=True,period_=MonthPeriod 2020 4}
+  let y2019report = multiBalanceReport d o2019 j2019
+  let y2020report = multiBalanceReport d o2020 j2020
+  --putStrLn $ multiBalanceReportAsText o2019 y2019report
+  --putStrLn $ multiBalanceReportAsText o2020 y2020report
+  let pastAsBudget = combineBudgetAndActual y2019report{prDates=prDates y2020report} y2020report 
+  putStrLn $ budgetReportAsText def pastAsBudget
